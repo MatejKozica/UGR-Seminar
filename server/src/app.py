630e4eef3feecd0fa4fd29d2b782d6b9c9582148
tkastelan <tonino9kas@gmail.com>
2024-01-22 21:37:49 +0100

    add templating and table

--- server/src/app.py
+++ server/src/app.py
@@ -1,19 +1,21 @@
-from typing import Optional
-
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pydantic import BaseModel
-# from fastapi.templating import Jinja2Templates
-from src.database.connections import db_get_connections, db_create_connection
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, Field
+
+from src.database.connections import db_get_connections, db_create_connection, remove_record
 from src.database.db_connection import conn
 
 security = HTTPBasic()
 app = FastAPI(dependencies=[Depends(security)])
-# templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
+templates = Jinja2Templates(directory="src/templates")
 
 class Connection(BaseModel):
-    value: str
+    value: str = Field(pattern=r'^[\w\s]+\s\d{3}-\d{4}$')
 
 users = {
     "admin": {
@@ -54,7 +56,7 @@ async def startup():
             CREATE TABLE IF NOT EXISTS connections(
                 id serial primary key,
                 name text,
-                university_identification_number integer,
+                university_identification_number text,
                 connection_time timestamp DEFAULT current_timestamp
             )
         """
@@ -63,7 +65,7 @@ async def startup():
 
 
 @app.get("/connections")
-async def getConnections(Verification = Depends(verification)):
+async def get_connections(Verification = Depends(verification)):
     if Verification:
         results = await db_get_connections()
         return {"code": 200, "data": results}
@@ -71,18 +73,33 @@ async def getConnections(Verification = Depends(verification)):
         return {"code": 401, "message": "Unauthorized"}
 
 @app.post("/connect")
-async def saveBluetoothConnection(connection: Connection, Verification = Depends(verification)):
-    print("Data: ", connection)
-    conn_split = connection.value.split(' ')
+async def save_bluetooth_connection(connection: Connection, Verification = Depends(verification)):
+    conn_split = connection.value.split()
     data = {
-        "name": conn_split[0],
-        "university_identification_number": int(conn_split[1])
+        "name": " ".join(conn_split[:-1]),
+        "university_identification_number": conn_split[-1]
     }
     if(Verification):
         results = await db_create_connection(data)
         return {"code": 200, "data": results}
     else:
         return {"code": 401, "message": "Unauthorized"}
+
+
+@app.get("/records")
+async def get_records(request: Request):
+    results = await db_get_connections()
+    return templates.TemplateResponse(
+        request=request, name="table.html", context={"items": results or []}
+    )
+
+@app.delete("/remove/{record_id}")
+async def delete_record(record_id: int):
+    await remove_record(record_id);
+    return 200
+
+
+
 
 
 @app.get("/ping")
--- server/src/database/connections.py
+++ server/src/database/connections.py
@@ -21,3 +21,8 @@ async def db_create_connection(connection):
 
         await cursor.execute(q, (connection["university_identification_number"], connection["name"]))
         return await cursor.fetchone()
+
+async def remove_record(record_id):
+    async with conn.cursor() as cursor:
+        q = f"""DELETE FROM connections WHERE id=%s"""
+        await cursor.execute(q, (record_id,))
